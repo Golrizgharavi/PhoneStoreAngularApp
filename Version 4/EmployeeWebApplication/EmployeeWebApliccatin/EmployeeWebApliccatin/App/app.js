ea49c97e7ba3b32484f5ec2a28c8a7a54940e638
Golriz <golriz.gharavi@icloud.com>
2019-01-02 21:43:56 -0500

    Home page is completed-top slider is completed. just small changes to design. :) !!

---
+++ Version 4/EmployeeWebApplication/EmployeeWebApliccatin/EmployeeWebApliccatin/App/app.js
@@ -0,0 +1,65 @@
+﻿
+
+var app = angular.module('myApp', ['ngRoute'])
+
+    .config(['$routeProvider', function ($routeProvider) {
+
+ 
+        $routeProvider
+            .when('/', {
+                templateUrl:'views/home.html',
+                controller:'homeController'
+
+            })
+
+            .when('/products', {
+                templateUrl: 'views/products.html',
+                controller: 'productsController'
+
+            })
+
+            .when('/phone', {
+                templateUrl: 'views/phone.html',
+                controller: 'phoneController'
+
+            })
+
+            .when('/contact', {
+                templateUrl: 'views/contact.html',
+                controller: 'contactController'
+
+            })
+            .when('/createEmployee', {
+                templateUrl: 'views/createEmployee.html',
+                controller: 'createEmployeeController'
+
+            })
+            .when('/employees', {
+                templateUrl: 'views/employees.html',
+                controller: 'employeesController'
+
+            })
+            .when('/updateEmployee', {
+                templateUrl: 'views/updateEmployee.html',
+                controller: 'updateEmployeeController'
+
+            })
+
+            .when('/phone/:id', {
+                templateUrl: 'views/phoneItem.html',
+                controller: 'phoneItemController'
+            })
+
+            .when('/test2', {
+                templateUrl: 'views/Test2.html',
+                controller: 'TestCtrl'
+
+            })
+            
+
+    }])
+
+    .controller('mainController', function ($scope) {
+      
+        $scope.message = "main content";
+    })
---
+++ Version 4/EmployeeWebApplication/EmployeeWebApliccatin/EmployeeWebApliccatin/App/controllers/phoneItemController.js
@@ -0,0 +1,68 @@
+﻿
+
+//$scope.id = $routeParams.id;
+'use strict'
+
+app.controller('phoneItemController', function ($routeParams, productService) {
+
+
+    var vm = this;
+    var MemoryItems = document.getElementsByClassName("MemItemInn");
+    var ColorItems = document.getElementsByClassName("ColorItemImg");
+   //alert($routeParams.id);
+
+    var promiseGet = productService.GetPhoneItembyID($routeParams.id);
+    
+
+    promiseGet.then(function (p1) {
+    vm.phoneItem = p1.data
+
+        vm.message = "Welcome to " + vm.phoneItem.Name +" Page";
+
+    },
+
+        function (errorP1) { alert('Failure Loading!' + errorP1) });
+
+    vm.SetSelection = function (obj,index) {
+
+        for (i = 0; i < obj.length; i++) {
+            obj[i].className = obj[i].className.replace(" ItemSelected", "");
+        }
+
+        obj[index - 1].className += " ItemSelected";
+    },
+    vm.GetPhoneColor = function (Id, index) {
+        
+        vm.SetSelection(ColorItems, index);
+
+        var GetPhoneColorResult = productService.GetPhoneFeatureInfo(Id);
+        GetPhoneColorResult.then(function (p1) {
+
+            vm.PhoneColor = p1.data;
+
+
+        },
+
+            function (errorP1) { alert('Failure Loading!' + errorP1) });
+
+        },
+
+        vm.GetPhoneMemory = function (Id, index) {
+
+        vm.SetSelection(MemoryItems, index);
+
+        var GetPhoneColorResult = productService.GetPhoneFeatureInfo(Id);
+        GetPhoneColorResult.then(function (p1) {
+
+            vm.PhoneColor = p1.data;
+
+
+        },
+
+            function (errorP1) { alert('Failure Loading!' + errorP1) });
+
+    }
+
+});
+
+
